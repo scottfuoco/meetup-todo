01e79eb930893eda0ddb95df7261c191ae1a7abd
Scott Fuoco <scottfuoco@Scotts-MBP.hitronhub.home>
2017-02-25 19:22:03 -0800

    fixed incomplete filter and seperated logic from component

--- src/containers/TodoList/index.js
+++ src/containers/TodoList/index.js
@@ -3,29 +3,33 @@ import React, { PropTypes } from 'react';
 import TodoBar from '../../components/TodoBar';
 import Todo from '../../components/Todo';
 
+const listTodos = (todos, filter, deleteTodo, toggleComplete) => {
+  return todos
+    .filter(todo => {
+      switch (filter) {
+        case 'none':
+          return todo;
+        case 'complete':
+          if (todo.complete === true)
+            return todo;
+          break;
+        case 'incomplete':
+          if (todo.complete === false)
+            return todo;
+          break;
+        default:
+          break;
+      }
+      return false;
+    })
+    .map(todo => <Todo key={todo.id} todo={todo} deleteTodo={deleteTodo} toggleComplete={toggleComplete} />)
+}
+
 const TodoList = ({ todos, toggleComplete, deleteCompleted, deleteTodo, handleFilterChange, filter }) => {
   return (
     <div>
       <TodoBar deleteCompleted={deleteCompleted} handleFilterChange={handleFilterChange} />
-      {todos
-        .filter(todo => {
-          switch (filter) {
-            case 'none':
-              return todo;
-            case 'complete':
-              if (todo.complete === true)
-                return todo;
-              break;
-            case 'uncomplete':
-              if (todo.complete === false)
-                return todo;
-              break;
-            default:
-              break;
-          }
-          return false;
-        })
-        .map(todo => <Todo key={todo.id} todo={todo} deleteTodo={deleteTodo} toggleComplete={toggleComplete} />)}
+      {listTodos(todos, filter, deleteTodo, toggleComplete)}
     </div>
   );
 };
